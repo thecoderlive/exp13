ada41915ac1b26c7f4ce2b951695b75c1ba6df61
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 02:06:32 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "1f479973-1c16-4fb6-a730-10ad3d93da8f",
+    "home_feed": [
+        {
+            "id": "4f62ea24-d35f-426d-9eef-b28cc84239fa",
+            "artist_name": "debate",
+            "home_carousel": [
+                {
+                    "id": "05270f64-0fe0-4de4-8a2b-c8f1dac184a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/91.png"
+                },
+                {
+                    "id": "1a6995dd-7490-4579-9a83-0a536e5ef1ae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/152.png"
+                },
+                {
+                    "id": "f98aaa5a-f396-497c-b88a-74fcae3adaf3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/340.png"
+                },
+                {
+                    "id": "b82d6499-6ce8-4862-815c-f0aff11a63e8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/160.png"
+                },
+                {
+                    "id": "f1611b16-5f1f-4a4e-ba06-64e23428ad94",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/342.png"
+                }
+            ],
+            "art_caption": "draw",
+            "art_description": "Campaign fight plant worry teacher rather simple enter."
+        },
+        {
+            "id": "90e7fb96-267e-4fab-a2dc-e45861b4b443",
+            "artist_name": "fight",
+            "home_carousel": [
+                {
+                    "id": "52ef42b5-7cbb-44eb-abf4-63c63e7794ce",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                },
+                {
+                    "id": "93e4e999-28fe-4882-8f6e-2845196349e5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/210.png"
+                },
+                {
+                    "id": "9f1eb171-4c8c-44d4-af69-1d938ed7d99e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png"
+                },
+                {
+                    "id": "24f505fb-c889-4a13-8995-f9e0cda7bb28",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/131.png"
+                },
+                {
+                    "id": "3144fe82-9bb2-4a83-9635-ef3bd343635c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                }
+            ],
+            "art_caption": "citizen",
+            "art_description": "Part page market machine since."
+        },
+        {
+            "id": "79bec21a-1ee6-47b7-8daa-e0ab2235c5c1",
+            "artist_name": "college",
+            "home_carousel": [
+                {
+                    "id": "76302851-8d87-4119-b8cc-5fa806d692cf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/239.png"
+                },
+                {
+                    "id": "c7e058b1-6413-467b-aa93-27fbe01f0638",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                },
+                {
+                    "id": "8f9d9393-dbab-4679-8c68-882a574dfef8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/242.png"
+                },
+                {
+                    "id": "01af39bd-4465-41af-87b3-7cd0eb41cc8d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/186.png"
+                },
+                {
+                    "id": "c7dda615-e74e-43b0-874c-7c30157208c7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/118.png"
+                }
+            ],
+            "art_caption": "physical",
+            "art_description": "Campaign fight plant worry teacher rather simple enter."
+        },
+        {
+            "id": "9cf8f815-4058-4034-ba00-75bedf7f2743",
+            "artist_name": "dog",
+            "home_carousel": [
+                {
+                    "id": "4001d033-dc1b-4b79-ad01-7d1eedb1f867",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/214.png"
+                },
+                {
+                    "id": "89ad66c9-c59b-4d01-aa56-e10fe9fc400d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png"
+                },
+                {
+                    "id": "8271d3e8-d79c-4164-83d1-bf2fb8304068",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/289.png"
+                },
+                {
+                    "id": "8b76b973-f329-481c-89bd-b4ec40da9bde",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png"
+                },
+                {
+                    "id": "f16b986d-b00f-4b23-83dd-5049ec0a56e4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/7.png"
+                }
+            ],
+            "art_caption": "note",
+            "art_description": "Hold fill head make. Wall course building include."
+        },
+        {
+            "id": "7af104d7-035d-4461-b429-c1e69645a32b",
+            "artist_name": "step",
+            "home_carousel": [
+                {
+                    "id": "b1dd6e46-95b5-4f3f-9d83-54ce7652b0a0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/120.png"
+                },
+                {
+                    "id": "715b17a0-c83d-4505-a916-53093fcd7097",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/199.png"
+                },
+                {
+                    "id": "2e45756f-d36f-42e4-a35e-46cb3bb322f0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/2.png"
+                },
+                {
+                    "id": "a064a747-1bf2-43cc-a961-be997183587c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/17.png"
+                },
+                {
+                    "id": "6c667d95-baf4-4b0d-9605-1345921ddd6a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png"
+                }
+            ],
+            "art_caption": "than",
+            "art_description": "Herself practice according cultural. Final writer tax huge decade must."
+        }
+    ]
+}
